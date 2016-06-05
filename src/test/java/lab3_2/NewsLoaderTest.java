562ff753c9184ea37f2e83d605ec9b6d5a010487
bulikd105 <bulikd105@gmail.com>
2016-06-05 18:24:35 +0200

    Dodanie testu dla subskrybentow

--- src/test/java/lab3_2/NewsLoaderTest.java
+++ src/test/java/lab3_2/NewsLoaderTest.java
@@ -2,6 +2,8 @@ package lab3_2;
 
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,9 +14,12 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import edu.iis.mto.staticmock.Configuration;
 import edu.iis.mto.staticmock.ConfigurationLoader;
+import edu.iis.mto.staticmock.IncomingInfo;
 import edu.iis.mto.staticmock.IncomingNews;
 import edu.iis.mto.staticmock.NewsLoader;
 import edu.iis.mto.staticmock.NewsReaderFactory;
+import edu.iis.mto.staticmock.PublishableNews;
+import edu.iis.mto.staticmock.SubsciptionType;
 import edu.iis.mto.staticmock.reader.NewsReader;
 
 @RunWith(PowerMockRunner.class)
@@ -22,6 +27,7 @@ import edu.iis.mto.staticmock.reader.NewsReader;
 public class NewsLoaderTest 
 {
 	private ConfigurationLoader configurationLoader;
+	private NewsReader newsReader;
 	
 	@Before
 	public void start() 
@@ -38,5 +44,25 @@ public class NewsLoaderTest
 		mockStatic(NewsReaderFactory.class);
 		when(NewsReaderFactory.getReader(Mockito.anyString())).thenReturn(newsReader);
 	}
-
+	
+	@Test
+	public void newsForSubscribersTest() 
+	{
+        IncomingNews incomingNews = new IncomingNews();
+        IncomingInfo incomingInfo = null;
+        
+        incomingInfo = new IncomingInfo("A type subscription", SubsciptionType.A);
+        incomingNews.add(incomingInfo);
+        
+        incomingInfo = new IncomingInfo("B type subscription", SubsciptionType.B);
+        incomingNews.add(incomingInfo);
+        
+        incomingInfo = new IncomingInfo("None type subscription", SubsciptionType.NONE);
+        incomingNews.add(incomingInfo);
+        
+        when(newsReader.read()).thenReturn(incomingNews);
+        NewsLoader newsLoader = new NewsLoader();
+        PublishableNews publishableNews = newsLoader.loadNews();
+        assertThat(publishableNews.getSubscribentContent().size(), is(2));
+	}
 }
