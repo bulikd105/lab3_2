09637c93fc77e24c1e1db3fb438cf354436de7fb
bulikd105 <bulikd105@gmail.com>
2016-06-05 17:58:56 +0200

    Dodanie klasy NewsLoaderTest

---
+++ src/test/java/lab3_2/NewsLoaderTest.java
@@ -0,0 +1,16 @@
+package lab3_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NewsLoaderTest 
+{
+
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
